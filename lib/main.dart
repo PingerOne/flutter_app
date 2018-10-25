0932e3fe4d58950d6c1d4288fc4fbc5b682fa5e4
Pinger <wanitme@gmail.com>
2018-10-25 23:01:26 +0800

    学习完第一篇flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,102 @@
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
+    // TODO: implement build
+
     return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+        title: "Flutter Demo",
+        theme: new ThemeData(primaryColor: Colors.orangeAccent),
+        home: new RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new RandomWordsState();
+  }
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final _suggestion = <WordPair>[];
+  final _bigFont = const TextStyle(fontSize: 18.0);
+
+  // 存储已经点击的单词
+  final _saved = <WordPair>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("Startup Name Generator"),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushPressed)
+        ],
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      body: _buildSuggestion(),
+    );
+  }
+
+  // 导航点击事件
+  void _pushPressed() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return new ListTile(
+            title: new Text(pair.asPascalCase, style: _bigFont));
+      });
+
+      final divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+
+      return new Scaffold(
+        appBar: new AppBar(title: new Text("Saved Suggestions")),
+        body: new ListView(children: divided),
+      );
+    }));
+  }
+
+  Widget _buildSuggestion() {
+    return new ListView.builder(
+        padding: EdgeInsets.all(16.0),
+        itemBuilder: (context, position) {
+          if (position.isOdd) return new Divider();
+
+          final index = position / 2;
+          if (index >= _suggestion.length) {
+            _suggestion.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestion[index.floor()]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    // 构造ui时 ，判断是否存储过
+    final _alreadySaved = _saved.contains(pair);
+
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _bigFont,
+      ),
+      trailing: new Icon(
+        _alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: _alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (_alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
@@ -90,7 +167,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             new Text(
-              'You have pushed the button this many times:',
+              'I just like you.',
             ),
             new Text(
               '$_counter',
